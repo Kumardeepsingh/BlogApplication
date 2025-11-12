d45eb36767afad21ad6ecb5e306c7ea71b804038
unknown <kumardeepsingh@student.kpu.ca>
2025-11-12 14:19:41 -0800

    Initial Commit

---
+++ backend/src/test/java/com/Kumar/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kumar.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
